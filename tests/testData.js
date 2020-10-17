533ca85923d329432ea938e5cead2bbb3cc771ce
백준하 <astoru@naver.com>
2020-10-17 23:12:47 +0900

    add test data for search

--- tests/testData.js
+++ tests/testData.js
@@ -161,7 +161,121 @@ const contentsOverview10 = {
     ]
 }
 
+const contents = [
+    {
+        "content_id" : "a234",
+        "title_kr" : "어벤져스",
+        "title_en" : "avengers",
+        "release_date" : "2012-02-12",
+        "content_type" : "movie",
+        "is_single" : true,
+        "poster_url" : "/test/posters/a234",
+        "top_words" : ["thor","ironman","thanos"],
+        "sub_type" : ["action"]
+    },
+    {
+        "content_id" : "b456",
+        "title_kr" : "스파이더",
+        "title_en" : "spiderman",
+        "release_date" : "2015-07-17",
+        "content_type" : "movie",
+        "is_single" : true,
+        "poster_url" : "/test/posters/b456",
+        "top_words" : ["spiderman","peter","tom"],
+        "sub_type" : ["action"]
+    },
+    {
+        "content_id" : "c789",
+        "title_kr" : "조커",
+        "title_en" : "joker",
+        "release_date" : "2020-01-01",
+        "content_type" : "movie",
+        "is_single" : true,
+        "poster_url" : "/test/posters/c789",
+        "top_words" : ["joker","joaquin","arthur"],
+        "sub_type" : ["thriller"]
+    },
+    {
+        "content_id" : "d123",
+        "title_kr" : "기생충",
+        "title_en" : "parasite",
+        "release_date" : "2020-01-01",
+        "content_type" : "movie",
+        "is_single" : true,
+        "poster_url" : "/test/posters/d123",
+        "top_words" : ["parasite","capitalism","cannes"],
+        "sub_type" : ["thriller"]
+    },
+    {
+        "content_id" : "e456",
+        "title_kr" : "인셉션",
+        "title_en" : "inception",
+        "release_date" : "2015-07-17",
+        "content_type" : "movie",
+        "is_single" : true,
+        "poster_url" : "/test/posters/e456",
+        "top_words" : ["nolan","zimmer","dicaprio"],
+        "sub_type" : ["action"]
+    },
+    {
+        "content_id" : "j901",
+        "title_kr" : "에이전트 오브 쉴드",
+        "title_en" : "Marvel's Agents of S.H.I.E.L.D.",
+        "release_date" : "2013-07-17",
+        "content_type" : "drama",
+        "is_single" : false,
+        "poster_url" : "/test/posters/j901",
+        "top_words" : ["coulson","melinda","skye"],
+        "sub_type" : ["action"]
+    },
+    {
+        "content_id" : "i678",
+        "title_kr" : "악마는 사라지지 않는다",
+        "title_en" : "The Devil All the Time",
+        "release_date" : "2020-07-17",
+        "content_type" : "drama",
+        "is_single" : false,
+        "poster_url" : "/test/posters/i678",
+        "top_words" : ["tom","holland","russel"],
+        "sub_type" : ["thriller"]
+    },
+    {
+        "content_id" : "h345",
+        "title_kr" : "에놀라 홈즈",
+        "title_en" : "enola holmes",
+        "release_date" : "2020-07-17",
+        "content_type" : "drama",
+        "is_single" : false,
+        "poster_url" : "/test/posters/h345",
+        "top_words" : ["enola","millie","holmes"],
+        "sub_type" : []
+    },
+    {
+        "content_id" : "g012",
+        "title_kr" : "셜록",
+        "title_en" : "sherlock",
+        "release_date" : "2010-07-17",
+        "content_type" : "drama",
+        "is_single" : false,
+        "poster_url" : "/test/posters/g012",
+        "top_words" : ["benedict","sherlock","moriarty"],
+        "sub_type" : []
+    },
+    {
+        "content_id" : "f789",
+        "title_kr" : "종이의 집",
+        "title_en" : "la casa de papel",
+        "release_date" : "2020-01-17",
+        "content_type" : "drama",
+        "is_single" : false,
+        "poster_url" : "/test/posters/f789",
+        "top_words" : ["tokio","rio","professor"],
+        "sub_type" : ["thriller"]
+    }
+]
+
 export default {
     contentsOverview,
-    contentsOverview10
+    contentsOverview10,
+    contents
 };
