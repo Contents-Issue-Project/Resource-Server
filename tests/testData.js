0a559413db12919bd0f9137e746392cca2c6bddf
백준하 <astoru@naver.com>
2020-11-04 22:19:40 +0900

    data structure with object id reference

--- tests/testData.js
+++ tests/testData.js
@@ -9,7 +9,7 @@ const contentsOverview = {
             "release_date" : "2012-02-12",
             "content_type" : "movie",
             "is_single" : true,
-            "poster_url" : "/test/posters/a234",
+            "poster_url" : "/test/posters/a234.jpg",
             "top_words" : ["thor","ironman","thanos"]
         },
         {
@@ -19,7 +19,7 @@ const contentsOverview = {
             "release_date" : "2015-07-17",
             "content_type" : "movie",
             "is_single" : true,
-            "poster_url" : "/test/posters/b456",
+            "poster_url" : "/test/posters/b456.jpg",
             "top_words" : ["spiderman","peter","tom"]
         },
         {
@@ -29,7 +29,7 @@ const contentsOverview = {
             "release_date" : "2020-01-01",
             "content_type" : "movie",
             "is_single" : true,
-            "poster_url" : "/test/posters/c789",
+            "poster_url" : "/test/posters/c789.jpg",
             "top_words" : ["joker","joaquin","arthur"]
         },
         {
@@ -39,7 +39,7 @@ const contentsOverview = {
             "release_date" : "2020-01-01",
             "content_type" : "movie",
             "is_single" : true,
-            "poster_url" : "/test/posters/d123",
+            "poster_url" : "/test/posters/d123.jpg",
             "top_words" : ["parasite","capitalism","cannes"]
         },
         {
@@ -49,7 +49,7 @@ const contentsOverview = {
             "release_date" : "2015-07-17",
             "content_type" : "movie",
             "is_single" : true,
-            "poster_url" : "/test/posters/e456",
+            "poster_url" : "/test/posters/e456.jpg",
             "top_words" : ["nolan","zimmer","dicaprio"]
         }
     ]
@@ -65,7 +65,7 @@ const contentsOverview10 = {
             "release_date" : "2012-02-12",
             "content_type" : "movie",
             "is_single" : true,
-            "poster_url" : "/test/posters/a234",
+            "poster_url" : "/test/posters/a234.jpg",
             "top_words" : ["thor","ironman","thanos"]
         },
         {
@@ -75,7 +75,7 @@ const contentsOverview10 = {
             "release_date" : "2015-07-17",
             "content_type" : "movie",
             "is_single" : true,
-            "poster_url" : "/test/posters/b456",
+            "poster_url" : "/test/posters/b456.jpg",
             "top_words" : ["spiderman","peter","tom"]
         },
         {
@@ -85,7 +85,7 @@ const contentsOverview10 = {
             "release_date" : "2020-01-01",
             "content_type" : "movie",
             "is_single" : true,
-            "poster_url" : "/test/posters/c789",
+            "poster_url" : "/test/posters/c789.jpg",
             "top_words" : ["joker","joaquin","arthur"]
         },
         {
@@ -95,7 +95,7 @@ const contentsOverview10 = {
             "release_date" : "2020-01-01",
             "content_type" : "movie",
             "is_single" : true,
-            "poster_url" : "/test/posters/d123",
+            "poster_url" : "/test/posters/d123.jpg",
             "top_words" : ["parasite","capitalism","cannes"]
         },
         {
@@ -105,7 +105,7 @@ const contentsOverview10 = {
             "release_date" : "2015-07-17",
             "content_type" : "movie",
             "is_single" : true,
-            "poster_url" : "/test/posters/e456",
+            "poster_url" : "/test/posters/e456.jpg",
             "top_words" : ["nolan","zimmer","dicaprio"]
         },
         {
@@ -115,7 +115,7 @@ const contentsOverview10 = {
             "release_date" : "2013-07-17",
             "content_type" : "drama",
             "is_single" : false,
-            "poster_url" : "/test/posters/j901",
+            "poster_url" : "/test/posters/j901.jpg",
             "top_words" : ["coulson","melinda","skye"]
         },
         {
@@ -125,7 +125,7 @@ const contentsOverview10 = {
             "release_date" : "2020-07-17",
             "content_type" : "drama",
             "is_single" : false,
-            "poster_url" : "/test/posters/i678",
+            "poster_url" : "/test/posters/i678.jpg",
             "top_words" : ["tom","holland","russel"]
         },
         {
@@ -135,7 +135,7 @@ const contentsOverview10 = {
             "release_date" : "2020-07-17",
             "content_type" : "drama",
             "is_single" : false,
-            "poster_url" : "/test/posters/h345",
+            "poster_url" : "/test/posters/h345.jpg",
             "top_words" : ["enola","millie","holmes"]
         },
         {
@@ -145,7 +145,7 @@ const contentsOverview10 = {
             "release_date" : "2010-07-17",
             "content_type" : "drama",
             "is_single" : false,
-            "poster_url" : "/test/posters/g012",
+            "poster_url" : "/test/posters/g012.jpg",
             "top_words" : ["benedict","sherlock","moriarty"]
         },
         {
@@ -155,13 +155,13 @@ const contentsOverview10 = {
             "release_date" : "2020-01-17",
             "content_type" : "drama",
             "is_single" : false,
-            "poster_url" : "/test/posters/f789",
+            "poster_url" : "/test/posters/f789.jpg",
             "top_words" : ["tokio","rio","professor"]
         }
     ]
 }
 
-const contents = [
+const contentDetails = [
     {
         "content_id" : "a234",
         "title_kr" : "어벤져스",
@@ -169,9 +169,33 @@ const contents = [
         "release_date" : "2012-02-12",
         "content_type" : "movie",
         "is_single" : true,
-        "poster_url" : "/test/posters/a234",
+        "poster_url" : "/test/posters/a234.jpg",
         "top_words" : ["thor","ironman","thanos"],
-        "sub_type" : ["action","fantasy"]
+        "sub_type" : ["action","fantasy"],
+
+        "is_hot" : true,
+
+        "single_statistics" : [
+            {
+                "statistics_type" : "wordcloud",
+                "statistics_name" : "wordcloud",
+                "url" : "/test/statistics/wordCloud.png"
+            },
+            {
+                "statistics_type" : "sampleStat",
+                "statistics_name" : "sampleStat",
+                "url" : "/test/statistics/sampleStat.png"
+            }
+        ],
+        "type_additional_data" : {
+            "casts" : ["robert downey jr", "chris hemsworth", "chris evans"],
+            "director" : "joss whedon",
+            "rating" : 8.5
+        },
+        "season_data" : {
+            "season_count" : 0, // single contents이므로.
+            "seasons" : []
+        }
     },
     {
         "content_id" : "b456",
@@ -180,9 +204,33 @@ const contents = [
         "release_date" : "2015-07-17",
         "content_type" : "movie",
         "is_single" : true,
-        "poster_url" : "/test/posters/b456",
+        "poster_url" : "/test/posters/b456.jpg",
         "top_words" : ["spiderman","peter","tom"],
-        "sub_type" : ["action", "fantasy"]
+        "sub_type" : ["action", "fantasy"],
+
+        "is_hot" : false,
+
+        "single_statistics" : [
+            {
+                "statistics_type" : "wordcloud",
+                "statistics_name" : "wordcloud",
+                "url" : "/test/statistics/wordCloud.png"
+            },
+            {
+                "statistics_type" : "sampleStat",
+                "statistics_name" : "sampleStat",
+                "url" : "/test/statistics/sampleStat.png"
+            }
+        ],
+        "type_additional_data" : {
+            "casts" : ["robert downey jr", "tom holland", "michael keaton"],
+            "director" : "john watts",
+            "rating" : 9.0
+        },
+        "season_data" : {
+            "season_count" : 0, // single contents이므로.
+            "seasons" : []
+        }
     },
     {
         "content_id" : "c789",
@@ -191,9 +239,33 @@ const contents = [
         "release_date" : "2020-01-01",
         "content_type" : "movie",
         "is_single" : true,
-        "poster_url" : "/test/posters/c789",
+        "poster_url" : "/test/posters/c789.jpg",
         "top_words" : ["joker","joaquin","arthur"],
-        "sub_type" : ["thriller","horror","crime"]
+        "sub_type" : ["thriller","horror","crime"],
+
+        "is_hot" : true,
+
+        "single_statistics" : [
+            {
+                "statistics_type" : "wordcloud",
+                "statistics_name" : "wordcloud",
+                "url" : "/test/statistics/wordCloud.png"
+            },
+            {
+                "statistics_type" : "sampleStat",
+                "statistics_name" : "sampleStat",
+                "url" : "/test/statistics/sampleStat.png"
+            }
+        ],
+        "type_additional_data" : {
+            "casts" : ["joaquin phoenix", "robert de niro"],
+            "director" : "todd phillips",
+            "rating" : 9.5
+        },
+        "season_data" : {
+            "season_count" : 0, // single contents이므로.
+            "seasons" : []
+        }
     },
     {
         "content_id" : "d123",
@@ -202,9 +274,33 @@ const contents = [
         "release_date" : "2020-01-01",
         "content_type" : "movie",
         "is_single" : true,
-        "poster_url" : "/test/posters/d123",
+        "poster_url" : "/test/posters/d123.jpg",
         "top_words" : ["parasite","capitalism","cannes"],
-        "sub_type" : ["thriller","drama","comedy"]
+        "sub_type" : ["thriller","drama","comedy"],
+
+        "is_hot" : true,
+
+        "single_statistics" : [
+            {
+                "statistics_type" : "wordcloud",
+                "statistics_name" : "wordcloud",
+                "url" : "/test/statistics/wordCloud.png"
+            },
+            {
+                "statistics_type" : "sampleStat",
+                "statistics_name" : "sampleStat",
+                "url" : "/test/statistics/sampleStat.png"
+            }
+        ],
+        "type_additional_data" : {
+            "casts" : ["song kang ho", "park so dam", "lee sun kyun", "cho yeo jeong"],
+            "director" : "bong joon ho",
+            "rating" : 10.0
+        },
+        "season_data" : {
+            "season_count" : 0, // single contents이므로.
+            "seasons" : []
+        }
     },
     {
         "content_id" : "e456",
@@ -213,9 +309,33 @@ const contents = [
         "release_date" : "2015-07-17",
         "content_type" : "movie",
         "is_single" : true,
-        "poster_url" : "/test/posters/e456",
+        "poster_url" : "/test/posters/e456.jpg",
         "top_words" : ["nolan","zimmer","dicaprio"],
-        "sub_type" : ["action","sci-fi"]
+        "sub_type" : ["action","sci-fi"],
+
+        "is_hot" : false,
+
+        "single_statistics" : [
+            {
+                "statistics_type" : "wordcloud",
+                "statistics_name" : "wordcloud",
+                "url" : "/test/statistics/wordCloud.png"
+            },
+            {
+                "statistics_type" : "sampleStat",
+                "statistics_name" : "sampleStat",
+                "url" : "/test/statistics/sampleStat.png"
+            }
+        ],
+        "type_additional_data" : {
+            "casts" : ["leonardo dicaprio", "ken watanabe", "joseph gordon levitt"],
+            "director" : "christopher nolan",
+            "rating" : 8.5
+        },
+        "season_data" : {
+            "season_count" : 0, // single contents이므로.
+            "seasons" : []
+        }
     },
     {
         "content_id" : "j901",
@@ -224,9 +344,24 @@ const contents = [
         "release_date" : "2013-07-17",
         "content_type" : "drama",
         "is_single" : false,
-        "poster_url" : "/test/posters/j901",
+        "poster_url" : "/test/posters/j901.jpg",
         "top_words" : ["coulson","melinda","skye"],
-        "sub_type" : ["action","drama","sci-fi"]
+        "sub_type" : ["action","drama","sci-fi"],
+
+        "is_hot" : false,
+
+        "single_statistics" : [
+
+        ],
+        "type_additional_data" : {
+            "casts" : ["clark gregg", "samuel l jackson"],
+            "director" : "joss whedon",
+            "rating" : 8.5
+        },
+        "season_data" : {
+            "season_count" : 2,
+            "seasons" : [ObjectId("5fa2550bd415daab6e296014"), ObjectId("5fa25b55d415daab6e296018")]
+        }
     },
     {
         "content_id" : "i678",
@@ -235,9 +370,24 @@ const contents = [
         "release_date" : "2020-07-17",
         "content_type" : "drama",
         "is_single" : false,
-        "poster_url" : "/test/posters/i678",
+        "poster_url" : "/test/posters/i678.jpg",
         "top_words" : ["tom","holland","russel"],
-        "sub_type" : ["thriller","crime"]
+        "sub_type" : ["thriller","crime"],
+
+        "is_hot" : true,
+
+        "single_statistics" : [
+
+        ],
+        "type_additional_data" : {
+            "casts" : ["tom holland"],
+            "director" : "antonio campos",
+            "rating" : 8.5
+        },
+        "season_data" : {
+            "season_count" : 1, //single season
+            "seasons" : [ObjectId("5fa2550bd415daab6e296014")]
+        }
     },
     {
         "content_id" : "h345",
@@ -246,9 +396,24 @@ const contents = [
         "release_date" : "2020-07-17",
         "content_type" : "drama",
         "is_single" : false,
-        "poster_url" : "/test/posters/h345",
+        "poster_url" : "/test/posters/h345.jpg",
         "top_words" : ["enola","millie","holmes"],
-        "sub_type" : ["crime","adventure","mystery"]
+        "sub_type" : ["crime","adventure","mystery"],
+
+        "is_hot" : false,
+
+        "single_statistics" : [
+
+        ],
+        "type_additional_data" : {
+            "casts" : ["millie bobby brown", "sam claflin", "henry cavil"],
+            "director" : "harry bradbeer",
+            "rating" : 7.5
+        },
+        "season_data" : {
+            "season_count" : 1, // single season
+            "seasons" : [ObjectId("5fa2550bd415daab6e296014")]
+        }
     },
     {
         "content_id" : "g012",
@@ -257,9 +422,24 @@ const contents = [
         "release_date" : "2010-07-17",
         "content_type" : "drama",
         "is_single" : false,
-        "poster_url" : "/test/posters/g012",
+        "poster_url" : "/test/posters/g012.jpg",
         "top_words" : ["benedict","sherlock","moriarty"],
-        "sub_type" : ["crime","adventure","mystery"]
+        "sub_type" : ["crime","adventure","mystery"],
+
+        "is_hot" : false,
+
+        "single_statistics" : [
+
+        ],
+        "type_additional_data" : {
+            "casts" : ["benedict cumberbatch", "martin freeman"],
+            "director" : "steven moffat",
+            "rating" : 8.5
+        },
+        "season_data" : {
+            "season_count" : 2,
+            "seasons" : [ObjectId("5fa2550bd415daab6e296014"), ObjectId("5fa25b55d415daab6e296018")]
+        }
     },
     {
         "content_id" : "f789",
@@ -268,14 +448,141 @@ const contents = [
         "release_date" : "2020-01-17",
         "content_type" : "drama",
         "is_single" : false,
-        "poster_url" : "/test/posters/f789",
+        "poster_url" : "/test/posters/f789.jpg",
         "top_words" : ["tokio","rio","professor"],
-        "sub_type" : ["thriller","crime","drama"]
+        "sub_type" : ["thriller","crime","drama"],
+
+        "is_hot" : true,
+
+        "single_statistics" : [
+
+        ],
+        "type_additional_data" : {
+            "casts" : ["ursula corbero", "alvaro morte"],
+            "director" : "alex pina",
+            "rating" : 9.5
+        },
+        "season_data" : {
+            "season_count" : 2,
+            "seasons" : [ObjectId("5fa2550bd415daab6e296014"), ObjectId("5fa25b55d415daab6e296018")]
+        }
+    }
+]
+
+
+// "season_data" : {
+//     "season_count" : 2,
+//         "seasons" : [
+//         {
+//             "$oid" : "5fa2550bd415daab6e296014"
+//         },
+//         {
+//             "$oid" : "5fa25b55d415daab6e296018"
+//         }
+//     ]
+// }
+//테스트용에서는 같은 데이터 링크 걸어놓으면 되기 때문에 episode 개수가 다른 2개만 만듬.
+const seasonDetails = [
+    {
+        "episode_count" :3,
+        "season_statistics" : [
+            {
+                "statistics_type" : "wordcloud",
+                "statistics_name" : "wordcloud for season1",
+                "url" : "/test/statistics/wordCloud.png"
+            },
+            {
+                "statistics_type" : "sampleStat",
+                "statistics_name" : "sampleStat for season1",
+                "url" : "/test/statistics/sampleStat.png"
+            }
+        ],
+        "episodes" : [ObjectId("5fa253c010e1484f9858b101"), ObjectId("5fa253d010e1484f9858b102"),
+        ObjectId("5fa253db10e1484f9858b103")]
+    },
+    {
+        "episode_count" :4,
+        "season_statistics" : [
+            {
+                "statistics_type" : "wordcloud",
+                "statistics_name" : "wordcloud for season1",
+                "url" : "/test/statistics/wordCloud.png"
+            },
+            {
+                "statistics_type" : "sampleStat",
+                "statistics_name" : "sampleStat for season1",
+                "url" : "/test/statistics/sampleStat.png"
+            }
+        ],
+        "episodes" : [ObjectId("5fa253c010e1484f9858b101"), ObjectId("5fa253d010e1484f9858b102"),
+            ObjectId("5fa253db10e1484f9858b103"), ObjectId("5fa253e310e1484f9858b104")]
+    }
+]
+
+//전부 같은 형태일 것. 다만 다른 object id 넣기 위해서 name만 다른 최대 4개의 복제본 생성해 사용함
+const leafEpisodes = [
+    {
+        "episode_statistics" : [
+            {
+                "statistics_type" : "wordcloud",
+                "statistics_name" : "wordcloud for episode1",
+                "url" : "/test/statistics/wordCloud.png"
+            },
+            {
+                "statistics_type" : "sampleStat",
+                "statistics_name" : "sampleStat for episode1",
+                "url" : "/test/statistics/sampleStat.png"
+            }
+        ]
+    },
+    {
+        "episode_statistics" : [
+            {
+                "statistics_type" : "wordcloud",
+                "statistics_name" : "wordcloud for episode2",
+                "url" : "/test/statistics/wordCloud.png"
+            },
+            {
+                "statistics_type" : "sampleStat",
+                "statistics_name" : "sampleStat for episode2",
+                "url" : "/test/statistics/sampleStat.png"
+            }
+        ]
+    },
+    {
+        "episode_statistics" : [
+            {
+                "statistics_type" : "wordcloud",
+                "statistics_name" : "wordcloud for episode3",
+                "url" : "/test/statistics/wordCloud.png"
+            },
+            {
+                "statistics_type" : "sampleStat",
+                "statistics_name" : "sampleStat for episode3",
+                "url" : "/test/statistics/sampleStat.png"
+            }
+        ]
+    },
+    {
+        "episode_statistics" : [
+            {
+                "statistics_type" : "wordcloud",
+                "statistics_name" : "wordcloud for episode4",
+                "url" : "/test/statistics/wordCloud.png"
+            },
+            {
+                "statistics_type" : "sampleStat",
+                "statistics_name" : "sampleStat for episode4",
+                "url" : "/test/statistics/sampleStat.png"
+            }
+        ]
     }
 ]
 
 export default {
     contentsOverview,
     contentsOverview10,
-    contents
+    contentDetails,
+    seasonDetails,
+    leafEpisodes
 };
