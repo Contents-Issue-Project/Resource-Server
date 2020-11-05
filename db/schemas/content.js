d0e6451ea7dd9f5c1893ba1e4c0c928b9d29a7ab
백준하 <astoru@naver.com>
2020-11-05 12:03:06 +0900

    modify content schema

--- db/schemas/content.js
+++ db/schemas/content.js
@@ -5,7 +5,7 @@ const contentSchema = new Schema({
     /*////////////////////////////////////////*/
     /*////////start of contents overview//////*/
     /*////////////////////////////////////////*/
-    content_id:{
+    content_id:{ // imdb
         type:String,
         required:true,
         unique:true
@@ -33,18 +33,26 @@ const contentSchema = new Schema({
         required : true
     },
     top_words : [{type : String, lowercase: true}],
+    sub_type : [String],
     /*////////////////////////////////////////*/
     /*/////////end of contents overview///////*/
     /*////////////////////////////////////////*/
-    sub_type : [String],
-    single_statistics : [String],
-    characters : [String],
 
-    type_additional_data : Schema.Types.Mixed,
-    series_additional_data : {
-        season_count : Number,
-        season_id : [Schema.Types.ObjectId]
-    }
+    type_additional_data : Schema.Types.Mixed, //content_type에 따라 다름
+
+    is_hot : Boolean,
+
+    season_data : { // is_single이 false일때만 적용.
+        season_count : Number
+    },
+
+    single_statistics : [ // is_single이 true일때만 적용.
+        {
+            statistics_type : String,
+            statistics_name : String,
+            url : String
+        }
+    ]
 });
 
 export default mongoose.model('Content', contentSchema);
